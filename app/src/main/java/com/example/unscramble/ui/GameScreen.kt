0dbabac250e9965f6435be3dd2a7520219c4810c
roshan <roshan.shinde164@gmail.com>
2024-06-01 20:36:34 +0530

    ui cosmetic changes

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -1,5 +1,6 @@
 package com.example.unscramble.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,22 +8,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.unscramble.R
 import com.example.unscramble.ui.theme.UnscrambleTheme
@@ -48,6 +56,7 @@ fun GameScreen() {
         GameLayout(
             modifier = Modifier
                 .fillMaxWidth()
+                .wrapContentHeight()
                 .padding(paddingMedium)
         )
         Button(
@@ -84,39 +93,50 @@ fun GameLayout(modifier: Modifier = Modifier) {
     val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
     val paddingSmall = dimensionResource(id = R.dimen.padding_small)
 
-    Card(modifier = modifier) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(paddingMedium),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(paddingMedium)
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
         ) {
-            Text(
-                text = stringResource(id = R.string.word_count_v_v, 0, 10),
-                style = MaterialTheme.typography.titleMedium,
+            Column(
+                verticalArrangement = Arrangement.spacedBy(paddingMedium),
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .padding(paddingSmall)
-                    .align(Alignment.End)
-            )
-            Text(
-                text = "Scrambled",
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Text(
-                text = stringResource(id = R.string.word_instruction),
-                style = MaterialTheme.typography.titleMedium
-            )
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text(text = stringResource(id = R.string.text_field_instruction)) },
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+                    .padding(paddingMedium)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.word_count_v_v, 0, 10),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = colorScheme.onPrimary,
+                    modifier = Modifier
+                        .clip(shapes.medium)
+                        .background(colorScheme.surfaceTint)
+                        .padding(paddingSmall)
+                        .align(Alignment.End)
+                )
+                Text(
+                    text = "Scrambled",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+                Text(
+                    text = stringResource(id = R.string.word_instruction),
+                    style = MaterialTheme.typography.titleMedium
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    shape = shapes.large,
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = colorScheme.surface,
+                        unfocusedContainerColor = colorScheme.surface,
+                        disabledContainerColor = colorScheme.surface
+                    ),
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = stringResource(id = R.string.text_field_instruction)) },
+                    isError = false
+                )
+            }
         }
-    }
 }
 
 @Composable
