681c6fc584e6e4fb55b4ae9cc87a4b4ab8ca3484
roshan <roshan.shinde164@gmail.com>
2024-06-01 19:17:48 +0530

    statusBarPadding, safeDrawings and verticalScroll added

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +35,9 @@ fun GameScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
+            .statusBarsPadding()
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState())
             .fillMaxWidth()
             .padding(paddingMedium)
     ) {
