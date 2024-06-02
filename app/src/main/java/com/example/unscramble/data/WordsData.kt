41d5a8ef128fc4387254593847a917776be82025
roshan <roshan.shinde164@gmail.com>
2024-06-02 14:21:04 +0530

    WordsData added

---
+++ app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -0,0 +1,34 @@
+package com.example.unscramble.data
+
+const val MAX_NO_OF_WORDS = 10
+const val POINTS_PER_WORD = 10
+
+val WordsData: Set<String> = setOf(
+    "bear",
+    "piano",
+    "whisper",
+    "earthquake",
+    "rain",
+    "seesaw",
+    "tree",
+    "penguin",
+    "fetch",
+    "stairs",
+    "bicycle",
+    "road",
+    "banana",
+    "newspaper",
+    "violin",
+    "money",
+    "think",
+    "green",
+    "tulips",
+    "garden",
+    "flower",
+    "kites",
+    "sunshine",
+    "blossoms",
+    "rainbow",
+    "butterfly",
+    "motorcycle"
+)
